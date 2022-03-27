4b747d82a1ba8b5c4f430c2746f65ea25aa0449a
Max Clark <78932576+Max-Clark@users.noreply.github.com>
2022-03-26 22:56:05 -0600

    fix: use new keyword to move to heap (#5)
    * chore: fix double hack with custom datatype
    
    * chore: add docs
    
    * chore: initial makefile work
    
    * feat: add makefile test runner
    
    * chore: fix matrix readme test instructions
    
    * chore: add compiler definition MAKE_TEST
    
    * chore: add readme
    
    * fix: stack overflow, move to heap
    
    * fix: testresult folder in gitignore, remove values

--- test/matrix/vectorProdLargeTest.cpp
+++ test/matrix/vectorProdLargeTest.cpp
@@ -12,17 +12,13 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    MPI_Init(&argc, &argv);
-    int rank, commSize = 0;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &commSize);
 
-    const size_t COLUMNS = 800;
+    const size_t COLUMNS = 2400;
     const size_t ROWS = COLUMNS;
     const size_t TOTAL = COLUMNS * ROWS;
 
-    double B[TOTAL];
-    double y[COLUMNS];
+    double *B = new double[TOTAL];
+    double *y = new double[COLUMNS];
 
     for (size_t i = 0; i < TOTAL; i++)
     {
@@ -36,6 +32,10 @@ int main(int argc, char **argv)
     random_shuffle(B, B + TOTAL);
     random_shuffle(y, y + COLUMNS);
 
+    MPI_Init(&argc, &argv);
+    int rank, commSize = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &commSize);
     MPI_Barrier(MPI_COMM_WORLD);
 
     auto start = high_resolution_clock::now();
@@ -48,7 +48,10 @@ int main(int argc, char **argv)
     {
         // cout << "Result is ";
         printVector(result, COLUMNS);
-        //cout << duration.count() << endl;
+
+#ifndef MAKE_TEST
+        cout << duration.count() << "us" << endl;
+#endif
     }
 
     MPI_Finalize();
