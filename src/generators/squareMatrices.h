ccc083a03a8cdf6e4da946108d1661f442abd4b2
Jaxton Winder <jjwinder9@gmail.com>
2022-04-17 19:37:32 -0600

    Added unit lower triangular generator

--- src/generators/squareMatrices.h
+++ src/generators/squareMatrices.h
@@ -12,6 +12,7 @@ using Matrix = std::vector<MathVector>;
 Matrix generateRandomMatrix(int n, double maxMagnitude=1.0);
 Matrix generateDiagonalMatrix(int n, double maxMagnitude=1.0);
 Matrix generateLowerTriangularMatrix(int n, double maxMagnitude=1.0);
+Matrix generateUnitLowerTriangularMatrix(int n, double maxMagnitude=1.0);
 Matrix generateUpperTriangularMatrix(int n, double maxMagnitude=1.0);
 
 #endif
